8584a68fe6d9da336ca734b2ec3e593080530570
anastasia <cyanidecalcium@yandex.ru>
2020-05-25 16:05:16 +0300

    minor

---
+++ cgi/without.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import cgi
+import sqlite3
+
+conn = sqlite3.connect("mydatabase.db")
+cursor = conn.cursor()
+
+form = cgi.FieldStorage()
+text1 = form.getfirst("TEXT_1", "no")
+text2 = form.getfirst("TEXT_2", "no")
+
+cursor.execute("SELECT secret FROM my WHERE login=? AND password = ?", (text1, text2))
+text3 = cursor.fetchall()
+
+print("Content-type: text/html\n")
+print("Login: {}".format(text1))
+print("Password: {}".format(text2))
+print("Secret: {}".format(text3))
